1052854776ae0892b7d379b7e707c00270453a5d
Caner <canerdk1995@hotmail.com>
2021-12-02 14:06:08 +0300

    created ordering.application operations with cqrs design

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -25,7 +25,8 @@ namespace Ordering.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            //services.AddApplicationServices();
+            //services.AddInfrastructureServices();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
